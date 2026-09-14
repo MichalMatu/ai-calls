d48e1afe48817ed621db7809ec6ee7dc31160bd1
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-09-14 15:21:46 +0200

    Add realtime client module build

---
+++ realtime-client/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "pl.michalmatu.aicallbridge.realtime"
+    compileSdk = 37
+
+    defaultConfig {
+        minSdk = 29
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation(project(":audio-bridge"))
+}
