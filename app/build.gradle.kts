1fc3e8e8a2cd5082f39cd74dd57d48eae7326cea
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-09-14 15:21:34 +0200

    Add Android app module build

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "pl.michalmatu.aicallbridge"
+    compileSdk = 37
+
+    defaultConfig {
+        applicationId = "pl.michalmatu.aicallbridge"
+        minSdk = 29
+        targetSdk = 37
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation(project(":audio-bridge"))
+    implementation(project(":realtime-client"))
+}
