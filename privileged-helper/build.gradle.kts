5ecc1a2850e06b791e104b4ea07698a4abae4843
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-09-16 03:15:10 +0200

    Create privileged helper Android module

---
+++ privileged-helper/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "pl.michalmatu.aicallbridge.helper"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 29
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
