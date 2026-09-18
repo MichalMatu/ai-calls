72b039d4da0c49305fefc90134d3d64591c421bb
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-09-18 12:43:31 +0200

    feat: add typed realtime function call event

--- realtime-client/src/main/java/pl/michalmatu/aicallbridge/realtime/RealtimeServerEvent.java
+++ realtime-client/src/main/java/pl/michalmatu/aicallbridge/realtime/RealtimeServerEvent.java
@@ -8,45 +8,53 @@ public final class RealtimeServerEvent {
         AUDIO_DELTA,
         REMOTE_SPEECH_STARTED,
         REMOTE_SPEECH_STOPPED,
+        FUNCTION_CALL,
         ERROR,
         OTHER,
     }
 
     private final Type type;
     private final PcmFrame audioFrame;
+    private final RealtimeFunctionCall functionCall;
     private final String errorMessage;
     private final String rawType;
 
     private RealtimeServerEvent(
         Type type,
         PcmFrame audioFrame,
+        RealtimeFunctionCall functionCall,
         String errorMessage,
         String rawType
     ) {
         this.type = type;
         this.audioFrame = audioFrame;
+        this.functionCall = functionCall;
         this.errorMessage = errorMessage;
         this.rawType = rawType;
     }
 
     static RealtimeServerEvent audioDelta(PcmFrame frame, String rawType) {
-        return new RealtimeServerEvent(Type.AUDIO_DELTA, frame, null, rawType);
+        return new RealtimeServerEvent(Type.AUDIO_DELTA, frame, null, null, rawType);
     }
 
     static RealtimeServerEvent speechStarted(String rawType) {
-        return new RealtimeServerEvent(Type.REMOTE_SPEECH_STARTED, null, null, rawType);
+        return new RealtimeServerEvent(Type.REMOTE_SPEECH_STARTED, null, null, null, rawType);
     }
 
     static RealtimeServerEvent speechStopped(String rawType) {
-        return new RealtimeServerEvent(Type.REMOTE_SPEECH_STOPPED, null, null, rawType);
+        return new RealtimeServerEvent(Type.REMOTE_SPEECH_STOPPED, null, null, null, rawType);
+    }
+
+    static RealtimeServerEvent functionCall(RealtimeFunctionCall call, String rawType) {
+        return new RealtimeServerEvent(Type.FUNCTION_CALL, null, call, null, rawType);
     }
 
     static RealtimeServerEvent error(String message, String rawType) {
-        return new RealtimeServerEvent(Type.ERROR, null, message, rawType);
+        return new RealtimeServerEvent(Type.ERROR, null, null, message, rawType);
     }
 
     static RealtimeServerEvent other(String rawType) {
-        return new RealtimeServerEvent(Type.OTHER, null, null, rawType);
+        return new RealtimeServerEvent(Type.OTHER, null, null, null, rawType);
     }
 
     public Type type() {
@@ -55,6 +63,10 @@ public final class RealtimeServerEvent {
 
     public PcmFrame audioFrame() {
         return audioFrame;
+    }
+
+    public RealtimeFunctionCall functionCall() {
+        return functionCall;
     }
 
     public String errorMessage() {
