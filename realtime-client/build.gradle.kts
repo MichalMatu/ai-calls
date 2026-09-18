cdba73e3de331e1e80cad33881e4b2b43d46ebfd
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-09-18 11:28:21 +0200

    build: keep okhttp compatible with compile sdk 36

--- realtime-client/build.gradle.kts
+++ realtime-client/build.gradle.kts
@@ -19,6 +19,6 @@ android {
 dependencies {
     implementation(project(":audio-bridge"))
     implementation("com.google.code.gson:gson:2.14.0")
-    implementation("com.squareup.okhttp3:okhttp:5.5.0")
+    implementation("com.squareup.okhttp3:okhttp:5.4.0")
     testImplementation("junit:junit:4.13.2")
 }
