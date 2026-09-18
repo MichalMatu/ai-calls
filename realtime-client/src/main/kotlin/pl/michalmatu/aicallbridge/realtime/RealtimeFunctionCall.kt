906e28fe7a5f970559e7a45a07bd2d3a68b2f359
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-09-18 16:52:17 +0200

    feat: retain realtime function response identity

--- realtime-client/src/main/kotlin/pl/michalmatu/aicallbridge/realtime/RealtimeFunctionCall.kt
+++ realtime-client/src/main/kotlin/pl/michalmatu/aicallbridge/realtime/RealtimeFunctionCall.kt
@@ -3,15 +3,19 @@ package pl.michalmatu.aicallbridge.realtime
 import com.google.gson.JsonParser
 
 /** Completed function call emitted by Realtime. Arguments are opaque to the transport layer. */
-data class RealtimeFunctionCall(
+data class RealtimeFunctionCall @JvmOverloads constructor(
     val callId: String,
     val name: String,
     val argumentsJson: String,
+    val responseId: String? = null,
 ) {
     init {
         require(callId.isNotBlank()) { "function call id must not be blank" }
         require(name.isNotBlank()) { "function call name must not be blank" }
         require(argumentsJson.isNotBlank()) { "function call arguments must not be blank" }
+        if (responseId != null) {
+            require(responseId.isNotBlank()) { "function call response id must be null or non-blank" }
+        }
         val parsed = try {
             JsonParser.parseString(argumentsJson)
         } catch (error: RuntimeException) {
@@ -21,5 +25,5 @@ data class RealtimeFunctionCall(
     }
 
     override fun toString(): String =
-        "RealtimeFunctionCall(callId=REDACTED, name=$name, arguments=REDACTED)"
+        "RealtimeFunctionCall(responseId=REDACTED, callId=REDACTED, name=$name, arguments=REDACTED)"
 }
