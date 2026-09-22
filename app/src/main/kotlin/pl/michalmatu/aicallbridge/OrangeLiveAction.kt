2ab7c32de3a0ab0eb590847c77c5420b9fc50b54
Michal Matuszewski <90936684+MichalMatu@users.noreply.github.com>
2026-09-22 11:51:30 +0200

    Add Orange caller ID restriction info action

--- app/src/main/kotlin/pl/michalmatu/aicallbridge/OrangeLiveAction.kt
+++ app/src/main/kotlin/pl/michalmatu/aicallbridge/OrangeLiveAction.kt
@@ -27,6 +27,7 @@ internal enum class OrangeLiveAction(
     VOICE_QUALITY_PROBLEM("voice_quality_problem", "Podczas rozmów zanika głos."),
     OUTGOING_CALL_PROBLEM("outgoing_call_problem", "Nie mogę wykonywać połączeń."),
     INCOMING_CALL_PROBLEM("incoming_call_problem", "Nie mogę odbierać połączeń."),
+    CALLER_ID_RESTRICTION_INFO("caller_id_restriction_info", "Jak działa zastrzeganie numeru?"),
     ROAMING_INFO("roaming_info", "Roaming."),
     ROAMING_PRICES("roaming_prices", "Chcę sprawdzić ceny w roamingu."),
     OBSERVE_ONLY("observe_only", null),
